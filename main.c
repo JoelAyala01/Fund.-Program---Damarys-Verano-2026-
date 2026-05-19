fcaae21fb8bbeaaaf3d3f04af8234c03eb05c4eb
Joel Ayala <140276353+JoelAyala01@users.noreply.github.com>
2026-05-19 17:19:35 -0400

    Refactor sudoku validation functions and input handling

--- main.c
+++ main.c
@@ -11,128 +11,163 @@ int verificar_subcuadro(int *, int);
 
 int main()
 {
-	int n;
-	do{
-		printf("\nIngresa la dimension del sudoku (4, 9, 16): ");
-		scanf("%d",&n);
-		if(n != 4 && n != 9 && n != 16)
-		{
-			printf("\n\n\aError!, Ingresa una dimension valida\n");
-			system("PAUSE");
-			system("CLS");
-		}
-	}while(n != 4 && n != 9 && n != 16);
+    int n;
+    do
+    {
+        printf("\nIngresa la dimension del sudoku (4, 9, 16): ");
+        scanf("%d",&n);
+        if(n != 4 && n != 9 && n != 16)
+        {
+            printf("\n\n\aError!, Ingresa una dimension valida\n");
+            system("PAUSE");
+            system("CLS");
+        }
+    }
+    while(n != 4 && n != 9 && n != 16);
 
-	int arreglo[n][n];
-	llenar_arr(&arreglo[0][0],n);
-	mostrar_arr(&arreglo[0][0],n);
+    int arreglo[n][n];
+    llenar_arr(&arreglo[0][0],n);
+    mostrar_arr(&arreglo[0][0],n);
 
-	system("PAUSE");
-	if(verificar_fila(&arreglo[0][0],n) == 1 && verificar_columna(&arreglo[0][0],n) == 1 && verificar_subcuadro(&arreglo[0][0],n) == 1)
-	{
-		printf("El sudoku es v%clido!\a\n",160);
-	}
-	else
-		printf("\nEl sudoku no es v%clido\n",160);
+    system("PAUSE");
+    if(verificar_fila(&arreglo[0][0],n) == 1 && verificar_columna(&arreglo[0][0],n) == 1 && verificar_subcuadro(&arreglo[0][0],n) == 1)
+    {
+        printf("El sudoku es v%clido!\a\n",160);
+    }
+    else
+        printf("\nEl sudoku no es v%clido\n",160);
 
     return 0;
 }
 
+/*
+Funcion: verificar_subcuadros.
+Objetivo: verificar que no se repiten valores en cada uno de los subcuadros.
+Retorna: retorna 1 si es valido y 0 si no lo es.
+*/
 int verificar_subcuadro(int *arr, int tam)
 {
-	int subcuadro = sqrt(tam);
+    int subcuadro = sqrt(tam);
 
-	for(int i=0; i<tam; i+=subcuadro) //i+=subcuadro equivale a i=i+subcuadro.
-	{
-		for(int j=0; j<tam; j+=subcuadro) ////j+=subcuadro equivale a j=j+subcuadro.
-		{
-			for(int x=0; x<subcuadro; x++)
-			{
-				for(int y=0; y<subcuadro; y++)
-				{
-					for(int x2=0; x2<subcuadro; x2++)
-					{
-						for(int y2=0; y2<subcuadro; y2++)
-						{
-							if(x2 == x && y2 == y)
-								continue;
-							if(*(arr+(i+x)*tam+(j+y)) == *(arr+(i+x2)*tam+(j+y2)))
-							{
-								printf("\nFallo en el subcuadro\n");
-								return 0;
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	return 1;
+    for(int i=0; i<tam; i+=subcuadro) //i+=subcuadro equivale a i=i+subcuadro.
+    {
+        for(int j=0; j<tam; j+=subcuadro) ////j+=subcuadro equivale a j=j+subcuadro.
+        {
+            for(int x=0; x<subcuadro; x++)
+            {
+                for(int y=0; y<subcuadro; y++)
+                {
+                    for(int x2=0; x2<subcuadro; x2++)
+                    {
+                        for(int y2=0; y2<subcuadro; y2++)
+                        {
+                            if(x2 == x && y2 == y)
+                                continue;
+                            if(*(arr+(i+x)*tam+(j+y)) == *(arr+(i+x2)*tam+(j+y2)))
+                            {
+                                printf("\nFallo en el subcuadro\n");
+                                return 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return 1;
 }
 
-//retorna 1 si es valido y 0 si no lo es.
+/*
+Funcion: verificar_columna.
+Objetivo: verificar que no se repiten valores en las columnas.
+Retorna: retorna 1 si es valido y 0 si no lo es.
+*/
 int verificar_columna(int *arr, int tam)
 {
-	for(int i=0; i<tam; i++)
-	{
-		for(int j=0; j<tam; j++)
-		{
-			for(int k=j+1; k<tam; k++)
-			{
-				if( *(arr + j*tam + i) == *(arr + k*tam + i))
-				{
-					return 0;
-				}
-			}
-		}
-	}
-	return 1;
+    for(int i=0; i<tam; i++)
+    {
+        for(int j=0; j<tam; j++)
+        {
+            for(int k=j+1; k<tam; k++)
+            {
+                if( *(arr + j*tam + i) == *(arr + k*tam + i))
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
 }
 
-//retorna 1 si es valido y 0 si no lo es.
+/*
+Funcion: verificar_fila.
+Objetivo: verificar que no se repiten valores en las filas.
+Retorna: retorna 1 si es valido y 0 si no lo es.
+*/
 int verificar_fila(int *arr, int tam)
 {
-	for(int i=0; i<tam; i++)
-	{
-		for(int j=0; j<tam; j++)
-		{
-			for(int k=j+1; k<tam; k++)
-			{
-				if( *(arr + i*tam + j) == *(arr + i*tam + k))
-				{
-					return 0;
-				}
-			}
-		}
-	}
-	return 1;
+    for(int i=0; i<tam; i++)
+    {
+        for(int j=0; j<tam; j++)
+        {
+            for(int k=j+1; k<tam; k++)
+            {
+                if( *(arr + i*tam + j) == *(arr + i*tam + k))
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
 }
 
+/*
+Funcion: mostrar_arr.
+Objetivo: mostrar la matriz en pantalla con los valores digitados por el usuario.
+Retorna: No retorna, es void.
+*/
 void mostrar_arr(int *arr, int tam)
 {
-	printf("\nEste es el sudoku (%dx%d): \n", tam,tam);
+    printf("\nEste es el sudoku (%dx%d): \n", tam,tam);
 
-	for(int i=0; i<tam; i++)
-	{
-		for(int j=0; j<tam; j++)
-		{
-			printf("%d ", *(arr + i*tam + j));
-		}
-		printf("\n");
-	}
+    for(int i=0; i<tam; i++)
+    {
+        for(int j=0; j<tam; j++)
+        {
+            printf("%d ", *(arr + i*tam + j));
+        }
+        printf("\n");
+    }
 }
 
+/*
+Funcion: llenar_arr.
+Objetivo: Llenar la matriz con los valores digitados por el usuario.
+Retorna: No retorna, es void.
+*/
 void llenar_arr(int *arr, int tam)
 {
-	printf("\nIngresa numeros del sudoku (%dx%d):\n", tam,tam);
+    printf("\nIngresa numeros del sudoku (%dx%d):\n", tam,tam);
 
-	for(int i=0; i<tam; i++)
-	{
-		for(int j=0; j<tam; j++)
-		{
-			printf("Valor [%d][%d]: ", i+1, j+1);
-			scanf("%d", (arr + i*tam + j) );
-		}
-		printf("\n");
-	}
+    for(int i=0; i<tam; i++)
+    {
+        for(int j=0; j<tam; j++)
+        {
+            do
+            {
+                printf("Valor [%d][%d]: ", i+1, j+1);
+                scanf("%d", (arr + i*tam + j) );
+                if(*(arr + i*tam + j) < 1 || *(arr + i*tam + j) > tam)
+				{
+					printf("\nValor fuera de rango\n");
+					system("PAUSE");
+					system("CLS");
+				}
+            }while(*(arr + i*tam + j) < 1 || *(arr + i*tam + j) > tam);
+
+        }
+        printf("\n");
+    }
 }
